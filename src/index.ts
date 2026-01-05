8ec4ae335f816834e7bddd0a7874c3003729ce01
Medo-ID <midaghdour@gmail.com>
2026-01-05 19:57:10 +0100

    deploy to vercel v4

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import express from 'express'
 import helmet from 'helmet'
-import cors from 'cors'
-import morgan from 'morgan'
-import { env, isTest } from '../env.ts'
-import { isAuthenticated } from './middlewares/auth.ts'
+// import cors from 'cors'
+// import morgan from 'morgan'
+// import { env, isTest } from '../env.ts'
+// import { isAuthenticated } from './middlewares/auth.ts'
 
 // // Routers Imports
-import { authRouter } from './routes/authRoutes.ts'
-import { userRouter } from './routes/userRoutes.ts'
-import { tagRouter } from './routes/tagRoutes.ts'
-import { habitRouter } from './routes/habitRoutes.ts'
-import { notFound } from './middlewares/notFound.ts'
-import { globalError } from './middlewares/globalError.ts'
-import { customRateLimiter } from './middlewares/rateLimiter.ts'
+// import { authRouter } from './routes/authRoutes.ts'
+// import { userRouter } from './routes/userRoutes.ts'
+// import { tagRouter } from './routes/tagRoutes.ts'
+// import { habitRouter } from './routes/habitRoutes.ts'
+// import { notFound } from './middlewares/notFound.ts'
+// import { globalError } from './middlewares/globalError.ts'
+// import { customRateLimiter } from './middlewares/rateLimiter.ts'
 import { homePageHTML } from './views/homePage.ts'
 import { renderDocsPage } from './views/docsPage.ts'
 
@@ -22,18 +22,18 @@ const NODE_ENV = process.env.NODE_ENV || 'development'
 
 // Regular middleware
 app.use(helmet())
-app.use(
-  cors({
-    origin: env.CORS_ORIGIN,
-    credentials: true,
-  })
-)
+// app.use(
+//   cors({
+//     origin: env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// )
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev', { skip: () => isTest() }))
+// app.use(morgan('dev', { skip: () => isTest() }))
 
 // // Custom rate limiter
-app.use(customRateLimiter)
+// app.use(customRateLimiter)
 
 // Health check endpoint
 app.get('/health', (req, res) => {
@@ -63,13 +63,13 @@ app.get('/docs', async (req, res) => {
 })
 
 // API Endpoints
-app.use('/api/auth', authRouter)
-app.use('/api/habits', isAuthenticated, habitRouter)
-app.use('/api/users', isAuthenticated, userRouter)
-app.use('/api/tags', isAuthenticated, tagRouter)
+// app.use('/api/auth', authRouter)
+// app.use('/api/habits', isAuthenticated, habitRouter)
+// app.use('/api/users', isAuthenticated, userRouter)
+// app.use('/api/tags', isAuthenticated, tagRouter)
 
 // 404 handler
-app.use(notFound)
+// app.use(notFound)
 
 // Global error handler
 // app.use(globalError)
