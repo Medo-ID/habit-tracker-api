2afcde5eff2feee45c6b1d848d8158348d810c5a
Medo-ID <midaghdour@gmail.com>
2026-01-05 19:47:59 +0100

    deploy to vercel v4

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from 'express'
 import helmet from 'helmet'
-// import cors from 'cors'
-// import morgan from 'morgan'
-// import { env, isTest } from '../env.ts'
-// import { isAuthenticated } from './middlewares/auth.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import { env, isTest } from '../env.ts'
+import { isAuthenticated } from './middlewares/auth.ts'
 
 // // Routers Imports
-// import { authRouter } from './routes/authRoutes.ts'
-// import { userRouter } from './routes/userRoutes.ts'
-// import { tagRouter } from './routes/tagRoutes.ts'
-// import { habitRouter } from './routes/habitRoutes.ts'
+import { authRouter } from './routes/authRoutes.ts'
+import { userRouter } from './routes/userRoutes.ts'
+import { tagRouter } from './routes/tagRoutes.ts'
+import { habitRouter } from './routes/habitRoutes.ts'
 import { notFound } from './middlewares/notFound.ts'
 import { globalError } from './middlewares/globalError.ts'
 import { customRateLimiter } from './middlewares/rateLimiter.ts'
@@ -22,15 +22,15 @@ const NODE_ENV = process.env.NODE_ENV || 'development'
 
 // Regular middleware
 app.use(helmet())
-// app.use(
-//   cors({
-//     origin: env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// )
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(morgan('dev', { skip: () => isTest() }))
+app.use(morgan('dev', { skip: () => isTest() }))
 
 // // Custom rate limiter
 app.use(customRateLimiter)
@@ -63,10 +63,10 @@ app.get('/docs', async (req, res) => {
 })
 
 // API Endpoints
-// app.use('/api/auth', authRouter)
-// app.use('/api/habits', isAuthenticated, habitRouter)
-// app.use('/api/users', isAuthenticated, userRouter)
-// app.use('/api/tags', isAuthenticated, tagRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/habits', isAuthenticated, habitRouter)
+app.use('/api/users', isAuthenticated, userRouter)
+app.use('/api/tags', isAuthenticated, tagRouter)
 
 // 404 handler
 app.use(notFound)
